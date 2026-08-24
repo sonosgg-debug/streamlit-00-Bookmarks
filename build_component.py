589c630f7ac3cff9f4e134c8e0ed401d16858f66
SSQ <sonosgg@gmail.com>
2026-08-24 21:21:16 +0900

    Inject cache-control meta tags in index.html to prevent Chrome from aggressively caching iframe resources

--- build_component.py
+++ build_component.py
@@ -27,6 +27,9 @@ html_template = """<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8" />
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Drag & Drop Bookmark Grid</title>
     <style>
